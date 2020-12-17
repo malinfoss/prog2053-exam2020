0f358412a29d04e223c123286ad58cc7d0535e91
Malin <malinfos@stud.ntnu.no>
2020-12-17 11:50:39 +0100

    Got the basic stuff down

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,57 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  constructor(){
+    super();
+    this.user = {
+      uname: '',
+      firstName: '',
+      lastName: '',
+      oldPwd: '',
+      pwd: ''
+    }
+  } 
+
+  static get styles() {
+    return css`
+      .row { display: flex; }
+      .column { width: 50%; padding: 100px; }
+      li { cursor: pointer; font-size: 22px; margin-bottom: 10px; }
+    `;
+  }
+
+  render(){
+    return html`
+      <ul>
+        <h2>Edit user information</h2>
+        <li>
+        <a>Username: </a> 
+        <input type = "text" placeholder = "${this.uname}">
+        </li>
+
+        <li>
+        <a>First name: </a>
+        <input type = "text" placeholder = "${this.firstName}">
+        </li>
+
+        <li>
+        <a>Last name: </a>
+        <input type = "text" placeholder = "${this.lastName}">
+        </li>
+
+        <h2>Change password</h2>
+        <li>
+        <a>New password: </a>
+        <input type = "text">
+        </li>
+
+        <li>
+        <a>Confirm password: </a>
+        <input type = "text">
+        </li>
+      </ul>
+    `
+  }
 
 }
 customElements.define('edit-user', EditUser);
